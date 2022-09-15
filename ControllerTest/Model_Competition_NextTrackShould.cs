b2d166ec2945b91ea00190d5bbc13377408b7078
MikeDriel <94164663+MikeDriel@users.noreply.github.com>
2022-09-15 12:39:05 +0200

    3.4 AF

--- ControllerTest/Model_Competition_NextTrackShould.cs
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -26,5 +26,42 @@ namespace ControllerTest
 			Track result = _competition.NextTrack();
 			Assert.IsNull(result);
 		}
+
+		[Test]
+		public void NextTrack_OneInQueue_ReturnTrack()
+		{
+			{
+				_competition.Tracks.Enqueue(new Track("TestTrack", new LinkedList<Section>()));
+				Track result = _competition.NextTrack();
+				Assert.AreEqual("TestTrack", result.Name);
+			}
+		}
+		
+		[Test]
+		public void NextTrack_OneInQueue_ReturnTrackFromQueue()
+		{
+
+			Track result;
+			
+				_competition.Tracks.Enqueue(new Track("TestTrack", new LinkedList<Section>()));
+				result = _competition.NextTrack();
+				result = _competition.NextTrack();
+				Assert.IsNull(result);
+			
+		}
+		
+		[Test]
+		public void NextTrack_TwoInQueue_ReturnNextTrack()
+		{
+
+			Track result;
+			
+			_competition.Tracks.Enqueue(new Track("TestTrack", new LinkedList<Section>()));
+			_competition.Tracks.Enqueue(new Track("TestTrack2", new LinkedList<Section>()));
+			result = _competition.NextTrack();
+			Assert.AreEqual("TestTrack", result.Name);
+			result = _competition.NextTrack();
+			Assert.AreEqual("TestTrack2", result.Name);
+		}
 	}
 }
