854ae2308bb1f74a58f99573f69aabeac14ed220
MikeDriel <94164663+MikeDriel@users.noreply.github.com>
2022-10-05 15:27:49 +0200

    tot 6.3

---
+++ WPFVisualize/PictureController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WPFVisualize
+{
+	public static class PictureController
+	{
+		public static Dictionary<string, Bitmap> ImageCache = new Dictionary<string, Bitmap>();
+
+
+		public static Bitmap GetBitmapUrl(string url)
+		{
+			if (ImageCache.ContainsKey(url))
+			{
+				return ImageCache[url];
+			}
+			else
+			{
+				Bitmap bitmap = new Bitmap(url);
+				ImageCache.Add(url, bitmap);
+				return bitmap;
+			}
+		}
+
+		public static void EmptyCache()
+		{
+			ImageCache.Clear();
+		}
+
+		public static Bitmap DrawOnBitmap(int x, int y)
+		{
+			if (ImageCache.ContainsKey("empty"))
+			{
+
+				Bitmap newBitmap = new Bitmap(x, y);
+				Graphics g = Graphics.FromImage(newBitmap);
+				
+				{
+					g.FillRegion(new SolidBrush(Color.White), new System.Drawing.Region(new Rectangle(0, 0, x, y)));
+
+					ImageCache.Add("empty", newBitmap);
+				return newBitmap;
+			}
+			return null;
+		}
+	}
+}
